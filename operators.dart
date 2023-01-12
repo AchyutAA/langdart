e633583271105f976bc57fec9661a5825cfe7e3a
AchyutAA <achyuta019314@nec.edu.np>
2023-01-12 23:29:36 +0545

    learn about operator in darts

---
+++ operators.dart
@@ -0,0 +1,17 @@
+main() {
+  ///operators
+  ///operator types
+  ///arithmetic operator,+.-,/,%,*;
+  ///
+  ///logic operator ,&&,||,!
+
+  ///comparision operator, <,>,<=,>=
+  ///ternary operator
+  ///condition ?..true:...false
+  int a = 5;
+  int b = 10;
+  (a > b) ? print("a is greater") : print("b is greater");
+
+  ///unary operator
+  ///a++,a--,
+}
